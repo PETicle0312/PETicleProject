0182cd67093c10c54cae9e02b50e6e4390322947
hyein kim <xzxz1006@gmail.com>
2025-06-04 11:34:08 +0900

    new

--- Front-end/admin/(tabs)/index.tsx
+++ Front-end/admin/(tabs)/index.tsx
@@ -1,27 +1,3 @@
 
-/*
-import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
-import AdminMainScreen from '../admin/admin_main';
-import AdminDetailsScreen from '../admin/admin_details/[school]';
-
-
-const Stack = createStackNavigator();
-*/
-
 export { default } from '../admin/admin_login';
-
-/*
-export default function TabIndex() {
-  return (
-
-      <Stack.Navigator initialRouteName="AdminMain">
-        <Stack.Screen name="AdminMain" component={AdminMainScreen} options={{headerShown:false}} />
-        <Stack.Screen name="AdminDetails" component={AdminDetailsScreen} />
-      </Stack.Navigator>
-
-  );
-
-  
-}
-  */
+//추가테스트 
